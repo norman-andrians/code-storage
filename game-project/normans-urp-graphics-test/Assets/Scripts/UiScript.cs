b2568001c96718cdc9f2dca1c658d75f726a23c5
norman-andrians <norman25.na@gmail.com>
2021-06-20 21:19:14 +0700

    adding color on fps ui

--- game-project/normans-urp-graphics-test/Assets/Scripts/UiScript.cs
+++ game-project/normans-urp-graphics-test/Assets/Scripts/UiScript.cs
@@ -8,6 +8,10 @@ public class UiScript : MonoBehaviour
 	public Text fpsText;
 	public Text posText;
 
+	public Color fpsBadColor;
+	public Color fpsNormalColor;
+	public Color fpsGoodColor;
+
 	public Transform playerPos;
 
     // Update is called once per frame
@@ -15,7 +19,18 @@ public class UiScript : MonoBehaviour
     {
     	float fps = 1/Time.deltaTime;
 
-        fpsText.text = fps.ToString("F2") + " FPS";
+        fpsText.text = fps.ToString() + " FPS";
         posText.text = "Location: (("+playerPos.position.x.ToString("F2")+"), ("+playerPos.position.y.ToString("F2")+"), ("+playerPos.position.z.ToString("F2")+"))";
+
+        if (fps < 10)
+        	fpsText.color = fpsBadColor;
+        else if (fps < 30)
+        	fpsText.color = fpsNormalColor;
+        else
+        	fpsText.color = fpsGoodColor;
+    }
+
+    public void QuitGame() {
+    	Application.Quit();
     }
 }
