3e9e668ab9a1312bcd9fb49fcea474cf7d8ed3af
norman-andrians <norman25.na@gmail.com>
2021-06-22 20:03:22 +0700

    fixed letter aneh

--- Code Project/Java/Yang Fang Katakan/whatwibusays/WibuSays.java
+++ Code Project/Java/Yang Fang Katakan/whatwibusays/WibuSays.java
@@ -14,7 +14,7 @@ public class WibuSays {
        String wibuBerkata1 = terjemahkan.faktanya + terjemahkan.saya + "mau ngomong " + terjemahkan.tapi;
        String wibuBerkata2 = terjemahkan.saya + "hanya sedang menghemat energi dari pada mengobrol dengan kalian ";
        String wibuBerkata3 = "yang " + terjemahkan.unknown1 + terjemahkan.omongKosong;
-       String wibuBerkata4 = "tapi " + terjemahkan.kamu + "nganggep " + terjemahkan.saya + "aneh" ;
+       String wibuBerkata4 = "tapi " + terjemahkan.kamu + "nganggep " + terjemahkan.saya + "aneh ";
        String wibuBerkata5 = terjemahkan.kamu + "pengen " + terjemahkan.saya + terjemahkan.bunuh;
        
        String fangBerkata = wibuBerkata1 + wibuBerkata2 + wibuBerkata3 + wibuBerkata4 + wibuBerkata5;
